e6b58be8ecb5305355659e96758ebf166894077f
Tea713 <thanhnguyen_2025@depauw.edu>
2025-01-13 00:18:52 +0700

    Add additional book entry attributes (pages and status)

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const libraryTable = document.getElementById("library");
 
 const myLibrary = [];
 
-function Book(title, author) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary(book) {
